a94d0de53f5227ed7c07001820a99c847ffaf225
dongsu.won <dongsu.won@lgepartner.com>
2025-12-02 16:49:33 +0900

    Fixed config and specs.
    Enovaui-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)

--- playwright.config.js
+++ playwright.config.js
@@ -16,6 +16,12 @@ export default defineConfig({
   testDir: './tests',
   testMatch: /.*.js/,
   /* Run tests in files in parallel */
+  snapshotPathTemplate: './snapshots/{projectName}/{platform}/{arg}{ext}',
+  expect: {
+    toHaveScreenshot: {
+      pathTemplate: './snapshots/{projectName}/{platform}/{arg}{ext}',
+    },
+  },
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -40,36 +46,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/primitive_tokens-spec.js
+++ tests/primitive_tokens-spec.js
@@ -6,8 +6,6 @@ import * as tokens from '../packages/core-tokens/json/color-primitive.json';
 // Also check that the color table is visible and has rows
 
 test('color tokens HTML renders and matches screenshot', async ({ page }) => {
-  const fs = require('fs');
-  const filePath = 'reference/color-tokens.png';
   let htmlContent = '<h1>header</h1><ul>';
  
   for (const [name, value] of Object.entries(tokens.primitive.color)) {
@@ -17,11 +15,5 @@ test('color tokens HTML renders and matches screenshot', async ({ page }) => {
   await page.setViewportSize({ width: 1280, height: 12000 });
   await page.setContent(htmlContent);
   await expect(page.locator('h1')).toHaveText('header');
-  if (fs.existsSync(filePath)) {
-    console.log('exisiting reference file');
-    await expect(await page.screenshot({fullPage: true})).toMatchSnapshot(filePath, {threshold: 0.1});
-  } else {
-    console.log('reference does not exist');
-    await page.screenshot({ path: filePath, fullPage: true });
-  }
+  await expect(page).toHaveScreenshot('reference-color-tockens.png', {threshold: 0.05});
 });
