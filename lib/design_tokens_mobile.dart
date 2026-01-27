5c1cf4e65ed5842cda7a6b2d7e9e8f502d2336ff
Juwon Jeong <juwon.jeong@lge.com>
2026-01-27 14:15:21 +0900

    Applied review

---
+++ lib/design_tokens_mobile.dart
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2025 LG Electronics Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export 'src/core_tokens/color_primitive.dart';
+export 'src/core_tokens/radius_primitive.dart';
+export 'src/core_tokens/spacing_primitive.dart';
+export 'src/core_tokens/typography_primitive.dart';
+export 'src/mobile_tokens/color_semantic.dart';
+export 'src/mobile_tokens/lg_brand/color/color_semantic_lg_brand.dart';
+export 'src/mobile_tokens/mobile/color/color_semantic_mobile.dart';
+export 'src/mobile_tokens/mono_black/color/color_semantic_mono_black.dart';
+export 'src/mobile_tokens/mono_white/color/color_semantic_mono_white.dart';
+export 'src/mobile_tokens/web/color/color_semantic_web.dart';
