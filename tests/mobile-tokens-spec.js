2b568c8e694227f4a8f4c05c97d6eeb9faa409d6
dongsuwon <61816767+dongsuwon@users.noreply.github.com>
2026-02-25 11:20:10 +0900

    * Fix typo.
    Enovaui-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)

--- tests/mobile-tokens-spec.js
+++ tests/mobile-tokens-spec.js
@@ -5,14 +5,14 @@ import * as colorSemanticMonoBlack from '../packages/mobile-tokens/json/color-se
 import * as colorSemanticMonoWhite from '../packages/mobile-tokens/json/color-semantic-mono-white.json';
 import * as colorSemanticWeb from '../packages/mobile-tokens/json/color-semantic-web.json';
 import * as colorSemanticMobile from '../packages/mobile-tokens/json/color-semantic-mobile.json';
-import * as colorSemanticLgBrand from '../packages/mobile-tokens/json/color-semantic-lg-brand.json';
+import * as colorSemanticLGBrand from '../packages/mobile-tokens/json/color-semantic-lg-brand.json';
 
 const cases = [
     {json: colorSemanticMonoBlack, title: 'Color semantic mono black tokens for mobile', file: 'reference-mobile-tokens-color-semantic-mono-black.png'},
     {json: colorSemanticMonoWhite, title: 'Color semantic mono white tokens for mobile', file: 'reference-mobile-tokens-color-semantic-mono-white.png'},
     {json: colorSemanticWeb, title: 'Color semantic web tokens for mobile', file: 'reference-mobile-tokens-color-semantic-web.png'},
     {json: colorSemanticMobile, title: 'Color semantic mobile tokens for mobile', file: 'reference-mobile-tokens-color-semantic-mobile.png'},
-    {json: colorSemanticLgBrand, title: 'Color semantic LG brand tokens for mobile', file: 'reference-mobile-tokens-color-semantic-lg-brand.png'},
+    {json: colorSemanticLGBrand, title: 'Color semantic LG brand tokens for mobile', file: 'reference-mobile-tokens-color-semantic-lg-brand.png'},
 ];
 
 for (const c of cases) {
--- tests/web-tokens-spec.js
+++ tests/web-tokens-spec.js
@@ -6,7 +6,7 @@ import * as colorSemanticMonoWhite from '../packages/web-tokens/json/color-seman
 import * as colorSemanticMobile from '../packages/web-tokens/json/color-semantic-mobile.json';
 import * as colorSemanticWeb from '../packages/web-tokens/json/color-semantic-web.json';
 import * as radiusSemantic from '../packages/web-tokens/json/radius-semantic.json';
-import * as colorSemanticLgBrand from '../packages/web-tokens/json/color-semantic-lg-brand.json';
+import * as colorSemanticLGBrand from '../packages/web-tokens/json/color-semantic-lg-brand.json';
 import * as typographySemantic from '../packages/web-tokens/json/typography-semantic.json';
 
 const cases = [
@@ -15,7 +15,7 @@ const cases = [
     {json: colorSemanticMobile, title: 'Color semantic mobile tokens for web', file: 'reference-web-tokens-color-semantic-mobile.png'},
     {json: colorSemanticWeb, title: 'Color semantic web tokens for web', file: 'reference-web-tokens-color-semantic-web.png'},
     {json: radiusSemantic, title: 'Radius semantic tokens for web', file: 'reference-web-tokens-radius-semantic.png'},
-    {json: colorSemanticLgBrand, title: 'Color semantic lg brand tokens for web', file: 'reference-web-tokens-color-semantic-lg-brand.png'},
+    {json: colorSemanticLGBrand, title: 'Color semantic lg brand tokens for web', file: 'reference-web-tokens-color-semantic-lg-brand.png'},
     {json: typographySemantic, title: 'Typography semantic tokens for web', file: 'reference-web-tokens-typography-semantic.png'},
 ];
 
