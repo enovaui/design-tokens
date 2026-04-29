a5df82efb527239eaf09d9d0b1a5788be1807254
Juwon Jeong <juwon.jeong@lge.com>
2026-04-29 12:39:15 +0900

    Add additional radius primitive tokens
    Enovaui-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

--- lib/src/core_tokens/radius_primitive.dart
+++ lib/src/core_tokens/radius_primitive.dart
@@ -17,7 +17,9 @@ class RadiusPrimitive {
   late final Radius radius8 = const Radius.circular(8);
   late final Radius radius10 = const Radius.circular(10);
   late final Radius radius12 = const Radius.circular(12);
+  late final Radius radius14 = const Radius.circular(14);
   late final Radius radius16 = const Radius.circular(16);
+  late final Radius radius18 = const Radius.circular(18);
   late final Radius radius20 = const Radius.circular(20);
   late final Radius radius24 = const Radius.circular(24);
   late final Radius radius28 = const Radius.circular(28);
@@ -25,10 +27,22 @@ class RadiusPrimitive {
   late final Radius radius32 = const Radius.circular(32);
   late final Radius radius36 = const Radius.circular(36);
   late final Radius radius40 = const Radius.circular(40);
+  late final Radius radius42 = const Radius.circular(42);
   late final Radius radius44 = const Radius.circular(44);
   late final Radius radius48 = const Radius.circular(48);
+  late final Radius radius52 = const Radius.circular(52);
   late final Radius radius54 = const Radius.circular(54);
+  late final Radius radius56 = const Radius.circular(56);
   late final Radius radius60 = const Radius.circular(60);
+  late final Radius radius66 = const Radius.circular(66);
+  late final Radius radius72 = const Radius.circular(72);
+  late final Radius radius78 = const Radius.circular(78);
+  late final Radius radius84 = const Radius.circular(84);
+  late final Radius radius90 = const Radius.circular(90);
+  late final Radius radius96 = const Radius.circular(96);
+  late final Radius radius102 = const Radius.circular(102);
   late final Radius radius108 = const Radius.circular(108);
+  late final Radius radius114 = const Radius.circular(114);
+  late final Radius radius120 = const Radius.circular(120);
   late final Radius radius999 = const Radius.circular(999);
 }
